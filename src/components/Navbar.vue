3451ea2ba7547fb6e3e0ff37515365361de4a96e
mr-zero <amirajubolchi001@gmail.com>
2020-09-11 18:16:02 +0530

    setting up google auth

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <div class="nav-wrapper">
+            <span class="brand-logo" >GeoChat</span>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data() {
+        return {};
+    },
+};
+</script>
