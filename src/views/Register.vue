0e61ad39d9e0315120dc6d8fdfb348d6f93479b9
mr-zero <amirajubolchi001@gmail.com>
2020-09-11 20:36:31 +0530

    Register components config

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,26 +2,31 @@
     <div class="container">
         <div class="row">
             <h3>Register</h3>
-            <form class="col s12">
+            <form class="col s12" @submit.prevent="register">
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">email</i>
-                        <input id="email" type="email" class="validate" />
+                        <input id="email" type="email" class="validate" v-model="email" />
                         <label for="email">Email</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">lock</i>
-                        <input id="password" type="password" class="validate" />
+                        <input id="password" type="password" class="validate" v-model="password" />
                         <label for="password">Password</label>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">lock</i>
-                        <input id="password2" type="password" class="validate" />
+                        <input id="password2" type="password" class="validate" v-model="repassword" />
                         <label for="password2">Re-Password</label>
+                    </div>
+                </div>
+                <div class="row" v-show="feedback">
+                    <div class="col s12">
+                        <p class="red-text" v-show="feedback">{{ feedback }}</p>
                     </div>
                 </div>
                 <button class="btn btn-large teal white-text">Login</button>
@@ -31,10 +36,29 @@
 </template>
 
 <script>
+// import firebase from "firebase";
 export default {
     name: "Register",
     data() {
-        return {};
+        return {
+            email: null,
+            password: null,
+            repassword: null,
+            feedback: null,
+        };
+    },
+    methods: {
+        register() {
+            if (this.email && this.password && this.repassword) {
+                if (this.password === this.repassword) {
+                    console.log(this.email, this.password, this.repassword);
+                } else {
+                    this.feedback = "Password are not similer.";
+                }
+            } else {
+                this.feedback = "Fill The Form.";
+            }
+        },
     },
 };
 </script>
