8072b9297fd7cae9c89a0efc7f0f7f897e517ebe
mr-zero <amirajubolchi001@gmail.com>
2020-09-11 16:30:27 +0530

    add firebase functional

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -2,27 +2,43 @@
     <div class="new-messages">
         <form action="" @submit.prevent="addMessage">
             <label for="new-messages">New Messages (enter to add): </label>
-            <input type="text" name="new-message"  v-model="newMessage"/>
+            <p class="red-text" v-show="feedback">{{ feedback }}</p>
+            <input type="text" name="new-message" v-model="newMessage" />
         </form>
     </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
 export default {
     name: "NewMessage",
     props: ["name"],
     data() {
         return {
             newMessage: null,
+            feedback: null,
         };
     },
     methods: {
         addMessage() {
-                console.log(this.newMessage, this.name, Date.now())
-            },
+            if (this.newMessage) {
+                db.collection("messages")
+                    .add({
+                        content: this.newMessage,
+                        name: this.name,
+                        timestamp: Date.now(),
+                    })
+                    .catch(() => {
+                        console.log("Error");
+                    });
+                this.newMessage = null;
+                this.feedback = null;
+            } else {
+                this.feedback = "Please enter some message.";
+            }
+        },
     },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
